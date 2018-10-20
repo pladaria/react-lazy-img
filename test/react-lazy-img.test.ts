2eca599714e554b06bc166792c10ad3181fba23f
Pedro Ladaria <pladaria@tuenti.com>
2018-10-20 20:53:11 +0200

    dummy test

--- test/react-lazy-img.test.ts
+++ test/react-lazy-img.test.ts
@@ -1,4 +1,4 @@
-import DummyClass from '../src/react-lazy-img';
+// import Img from '../src/react-lazy-img';
 
 /**
  * Dummy test
@@ -6,9 +6,5 @@ import DummyClass from '../src/react-lazy-img';
 describe('Dummy test', () => {
     it('works if true is truthy', () => {
         expect(true).toBeTruthy();
-    });
-
-    it('DummyClass is instantiable', () => {
-        expect(new DummyClass()).toBeInstanceOf(DummyClass);
     });
 });
